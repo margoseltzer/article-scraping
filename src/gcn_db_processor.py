4405a38bfcfd4bc55690c14e884366e069d4ff73
yuubd <gc.kim93@gmail.com>
2019-07-18 21:49:08 -0700

    obj props and rels implemented

--- src/gcn_db_processor.py
+++ src/gcn_db_processor.py
@@ -2,6 +2,7 @@ import codecs
 import argparse
 import json
 import os
+import itertools
 import CPL
 from CPL import ENTITY, AGENT, WASATTRIBUTEDTO, WASGENERATEDBY, WASDERIVEDFROM, BUNDLE_RELATION
 import string
@@ -14,23 +15,75 @@ db_connection = CPL.cpl_connection()
 # use 'test' for object and bundles
 
 def main():
+    # bundles = 145
+    # total nodes = 2620
     roots = db_connection.get_all_objects('test')
     bundles = [r.object for r in roots]
-    objects = []
-    relations = []
+    obj_props = []
+    rel_lists = [db_connection.get_bundle_relations(b) for b in bundles]
+    rel_list = list(itertools.chain.from_iterable(rel_lists))
     
-    b_obj_ids = []
+    obj_dict = {}
     for b in bundles:
         if type(b) == list: continue
-        #b_objs is list of {id: int}
-        id_objs = db_connection.get_bundle_objects(b)
-        ids = [obj.id for obj in id_objs]
-        print(ids)
-        b_obj_ids += ids
-    print(len(b_obj_ids))
-    print(len(set(b_obj_ids)))
-    print(len(roots))
+        b_objs = db_connection.get_bundle_objects(b)
+
+        for obj in b_objs:
+            if not obj.id in obj_dict:
+                obj_dict[obj.id] = obj.properties() 
+        
+    print(len(obj_dict.values()))
+    print(obj_dict)
+    print(123 + 'asd')
+    
+
+    # Get all relations and construct dicts {id:[ids]}
+    art_adj_dict = {}
+    agn_adj_dict = {}
+    qot_adj_dict = {}
+    for r in rel_list:
+
+        # type 8 = entity to entity, 'wasDerivedFrom'
+        if r.type == 8: 
+            art_adj_dict[r.base.id]  = art_adj_dict[r.base.id] + [r.other.id] if r.base.id  in art_adj_dict else [r.other.id]
+            art_adj_dict[r.other.id] = art_adj_dict[r.other.id] + [r.base.id] if r.other.id in art_adj_dict else [r.base.id]
+
+        # type 11 = entity to agent, or activity to agent 'wasAttributedTo'
+        elif r.type == 11 :
+            # and check of base is type 'article'
+            agn_adj_dict[r.base.id] = agn_adj_dict[r.base.id] + [r.other.id] if r.base.id in agn_adj_dict else [r.other.id]
+        
+        # type 9 = entity to quote, 'wasGeneratedBy'
+        elif r.type == 9:
+            qot_adj_dict[r.base.id] = qot_adj_dict[r.base.id] + [r.other.id] if r.base.id in qot_adj_dict else [r.other.id]
+            
+    # Check if all object is 
+    
+    sum = 0
+    for lst in art_adj_dict.values():
+        sum += len(lst)
+    for lst in agn_adj_dict.values():
+        sum += len(lst)
+    for lst in qot_adj_dict.values():
+        sum += len(lst)    
+    print(sum)
+
+    # Remove all duplicates
+    art_adj_dict = {k_id: list(set(v_ids)) for (k_id, v_ids) in art_adj_dict.items()}
+    agn_adj_dict = {k_id: list(set(v_ids)) for (k_id, v_ids) in agn_adj_dict.items()}
+    qot_adj_dict = {k_id: list(set(v_ids)) for (k_id, v_ids) in qot_adj_dict.items()}
+    
+    sum = 0
+    for lst in art_adj_dict.values():
+        sum += len(lst)
+    for lst in agn_adj_dict.values():
+        sum += len(lst)
+    for lst in qot_adj_dict.values():
+        sum += len(lst)    
+    print(sum)
+
     print('DONE')
+
 main()
 
 db_connection.close()
