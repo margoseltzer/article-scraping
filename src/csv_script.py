efef4b66e58cd3164b49789777ad8a010eae58c5
root <root@leap-514.maas.vm.cs.ubc.ca>
2019-08-27 02:30:25 +0000

    processed csv

--- src/csv_script.py
+++ src/csv_script.py
@@ -1,15 +1,29 @@
 import csv
 import os
-import numpy as np
-import pandas as pd
-
 
 path = os.path.dirname(os.path.realpath(__file__))
 dirpath = os.path.dirname(path)
-fakenews_path = os.path.dirname(dirpath)
-fake_csv_path = fakenews_path + 'fivek_fake.csv'
-real_csv_path = fakenews_path + 'fivek_real.csv'
+labeled_path = dirpath + '/data/datasets/labeled_articles.csv'
+print(labeled_path)
 
-with open(dirpath + 'obj_dict.csv', mode='r') as f: 
-    reader = csv.reader(f)
-    print(reader[0])
+fakenews_path = os.path.dirname(dirpath)
+fake_csv_path = fakenews_path + '/fivek_fake.csv'
+real_csv_path = fakenews_path + '/fivek_real.csv'
+
+reader = csv.reader(open(fake_csv_path, mode='r'))
+f_data = list(reader)
+print(f_data[2][5])
+
+reader = csv.reader(open(real_csv_path, mode='r'))
+r_data = list(reader)
+print(r_data[2][5])
+
+
+with open(labeled_path, mode='a') as csv_f:
+    w = csv.writer(csv_f)
+    for r in f_data:
+        w.writerow([r[5], 0])
+    
+    for r in r_data:
+        w.writerow([r[5], 1])
+
